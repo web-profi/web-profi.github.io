0af3b7f77fe449d5ca52d8af5d12864686a01e48
Sasha <sasha4474@ukr.net>
2020-11-10 10:52:12 +0200

    my-commit

--- js/main.js
+++ js/main.js
@@ -157,7 +157,21 @@ $(document).ready(function() {
     });
 });
 
-// $(function($){
-//     $('[name="phone"]').mask("+38(077) 777-7777");
-// });
+(function ($) {
+    $(function () {
+      $("ul.tabs__caption").on("click", "li:not(.active)", function () {
+        $(this)
+          .addClass("active")
+          .siblings()
+          .removeClass("active")
+          .closest("div.tabs")
+          .find("div.tabs__content")
+          .removeClass("active")
+          .eq($(this).index())
+          .addClass("active");
+      });
+    });
+  })(jQuery);
+
+
 
